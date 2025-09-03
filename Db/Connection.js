fcaca8bdf6e2e5a089caaf693e116c28cd87c4a2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-09-03 15:33:03 +0500

    Update Connection.js

--- Db/Connection.js
+++ Db/Connection.js
@@ -1,15 +1,13 @@
 // Import the mongoose library to interact with MongoDB
 import mongoose from 'mongoose'
-
-// Import the MongoDB connection URL from a config file
-import { Mongo_url } from './Config.js'
-
 // This function is responsible for establishing a connection to MongoDB
 export const DB_CONNECTED = async () => {
   try {
     // Use mongoose.connect() to connect to MongoDB
     // Mongo_url is the connection string (e.g., "mongodb://localhost:27017/internDB")
-    const data = await mongoose.connect(Mongo_url)
+    const data = await mongoose.connect(
+      'mongodb+srv://hamzahussain14hh:hamza123@internship.1bjsm13.mongodb.net/'
+    )
 
     // If connection is successful, log a success message
     if (data) console.log('✅ DB CONNECTED')
